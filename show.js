ec9470c1238f92fdd9626c5ff6c0372d41d84938
JinGye <19210240232@fudan.edu.cn>
2019-07-18 21:07:56 +0800

    右边画圈

--- show.js
+++ show.js
@@ -1,8 +1,11 @@
-function readMD(){
-	
+function parseTitle(content){
+	var r = /(<h2.*?h2>)/;
+	r.exec(content);
+	return RegExp.$1;
+}
 
-
-
-
-
+function parseContent(content){
+	var r = /}(.*?)/;
+	r.exec(content);
+	return RegExp.$1;
 }
